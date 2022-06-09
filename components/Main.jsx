a765a565befd62c5a17db5f55b587147e3873741
FrekiManagarm <mathieu.chambaud@ynov.com>
2022-06-09 13:58:08 +0200

    begin main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className='w-full h-screen text-center'>
-        <div className='max-w-[1240px] w-full mx-auto'>
+    <div id='home' className='w-full h-screen text-center'>
+        <div className='max-w-[1240px] w-full mx-auto p-2 justify-center items-center'>
             <div>
-
+                <p className='uppercase text-sm tracking-widest text-gray-600'>
+                    LET'S BUILD SOMETHING TOGETHER
+                </p>
+                <h1 className='py-4 text-gray-700'>
+                    Bonjour, Moi c'est <span className='text-[#5651e5]'>Mathieu</span>
+                </h1>
+                <h1 className='py-2 text-gray-700'>Je suis Développeur Full-Stack Web et Mobile</h1>
+                <p>
+                    
+                </p>
             </div>
         </div>
     </div>
