a449f2b3dfc3d645956c88d8bf3c0f0b264c55db
FrekiManagarm <mathieu.chambaud@ynov.com>
2022-10-27 16:29:22 +0200

    refactor mathieu chambaud portfolio

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['https://i.imgur.com', 'i.imgur.com', '*'],
-},
-  // env: {
-  //   PUBLIC_URL: process.env.PUBLIC_URL,
-  // }
+  swcMinify: true,
 }
 
 module.exports = nextConfig
